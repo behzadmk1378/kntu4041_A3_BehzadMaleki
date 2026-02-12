2e8da58eb69e0852392c6b7295230b3968adf664
Behzad <behzadmk1378@gmail.com>
2026-02-12 20:47:04 +0330

    GeoServer WMS Layer

--- static/js/map.js
+++ static/js/map.js
@@ -3,6 +3,25 @@
  * Uses OpenLayers library for web mapping functionality
  */
 
+// Create WMS source for GeoServer layer
+// This source will be used for both display and GetFeatureInfo queries
+const wmsSource = new ol.source.TileWMS({
+    // Public GeoServer WMS endpoint (for testing)
+    // Replace with your own GeoServer URL: 'http://your-server/geoserver/wms'
+    url: 'https://ahocevar.com/geoserver/wms',
+    params: {
+        'LAYERS': 'topp:states',  // Workspace:LayerName format
+        'TILED': true  // Request tiled images for better performance
+    },
+    serverType: 'geoserver',  // Optimizes requests for GeoServer
+    transition: 0  // No fade-in transition for tiles
+});
+
+// Create WMS layer from the source
+const wmsLayer = new ol.layer.Tile({
+    source: wmsSource
+});
+
 // Initialize the OpenLayers map
 const map = new ol.Map({
     target: 'map',  // HTML element ID where map will be rendered
@@ -11,13 +30,16 @@ const map = new ol.Map({
         // Provides the background map with streets, buildings, etc.
         new ol.layer.Tile({
             source: new ol.source.OSM()
-        })
+        }),
+        
+        // WMS Layer from GeoServer (on top of base layer)
+        wmsLayer
     ],
     view: new ol.View({
-        // Set initial map center to Tehran, Iran
+        // Set initial map center to USA (to see the states layer)
         // fromLonLat converts [longitude, latitude] to map projection (EPSG:3857)
-        center: ol.proj.fromLonLat([51.388974, 35.689198]),
-        zoom: 12  // Initial zoom level (0 = world view, higher = more zoomed in)
+        center: ol.proj.fromLonLat([-100, 40]),  // Center of USA
+        zoom: 4  // Zoom level to see multiple states
     })
 });
 
